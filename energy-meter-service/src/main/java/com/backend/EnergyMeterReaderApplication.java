40b373b09103a71b476e0d3ad5acb4b54e08c7fb
Sourabh Raghav <34327305+sourabhraghav2@users.noreply.github.com>
2021-04-13 11:05:57 +0700

    Linked energy meter app to configuration server

--- energy-meter-service/src/main/java/com/backend/EnergyMeterReaderApplication.java
+++ energy-meter-service/src/main/java/com/backend/EnergyMeterReaderApplication.java
@@ -1,12 +1,23 @@
 package com.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class EnergyMeterReaderApplication {
+    @Value("${description}")
+    String description;
+    @GetMapping("/test")
+    public String test(){
+        return description+" working!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EnergyMeterReaderApplication.class,args);
     }
